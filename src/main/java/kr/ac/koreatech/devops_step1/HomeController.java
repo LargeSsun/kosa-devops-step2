0193106ea5482409a803903f197294b41d3d37d0
LargeSsun <hyunho31313@gmail.com>
2025-12-19 09:57:56 +0900

    Update CI/CD workflow to include nginx.conf in sparse checkout and add environment variables for app1 and app2

--- src/main/java/kr/ac/koreatech/devops_step1/HomeController.java
+++ src/main/java/kr/ac/koreatech/devops_step1/HomeController.java
@@ -1,6 +1,8 @@
 package kr.ac.koreatech.devops_step1;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,8 +11,13 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 public class HomeController {
+
+    @Value("${APP_NAME:default_app}")
+    private String appName;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("name", appName);
         log.info("hello world");
         return "index.html";
     }
